bb6dcd005b8d239ae4e2dc4591b468e2e3a153cb
bhavyejain <bhavyej@gmail.com>
2023-03-16 16:51:12 -0700

    Add subroutine for clearing out network ports

---
+++ freeports.py
@@ -0,0 +1,22 @@
+import subprocess
+import re
+import os
+
+def get_pids(port):
+	command = "sudo lsof -i :%s | awk '{print $2}'" % port
+	pids = subprocess.check_output(command, shell=True)
+	pids = pids.strip()
+	if pids:
+		pids = re.sub(' +', ' ', pids)
+		for pid in pids.split('\n'):
+			try:
+				yield int(pid)
+			except:
+				pass
+
+def free_ports(ports):
+    for port in ports:
+        pids = set(get_pids(port))
+        for pid in pids:
+            command = f'sudo kill -9 {str(pid)}'
+            os.system(command)
